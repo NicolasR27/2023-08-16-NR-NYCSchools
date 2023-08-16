466d99563a07b1030c0fb9a6e1c9568b5709b776
Nicolas Rios <nicolas.rios25@yahoo.com>
2023-08-16 16:11:50 -0700

    Initial Commit

---
+++ 2023-08-16-NR-NYCSchools/_023_08_16_NR_NYCSchoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _023_08_16_NR_NYCSchoolsApp.swift
+//  2023-08-16-NR-NYCSchools
+//
+//  Created by Nicolas Rios on 8/16/23.
+//
+
+import SwiftUI
+
+@main
+struct _023_08_16_NR_NYCSchoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
