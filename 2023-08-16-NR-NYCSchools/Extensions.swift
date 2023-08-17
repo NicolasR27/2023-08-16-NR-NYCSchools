1d1621a916f03577e5c77265f88dd6041418acd5
Nicolas Rios <nicolas.rios25@yahoo.com>
2023-08-16 21:43:32 -0700

    finished code challenge

---
+++ 2023-08-16-NR-NYCSchools/Extensions.swift
@@ -0,0 +1,9 @@
+
+import SwiftUI
+
+extension Color {
+    static let publicNavy = Color("publicNavy"),
+               nycGreen = Color("nycGreen"),
+               schoolOrange = Color("schoolOrange"),
+               boardBlue = Color("boardBlue")
+}
---
+++ 2023-08-16-NR-NYCSchools/Views/SchooListItem.swift
@@ -0,0 +1,23 @@
+
+
+import SwiftUI
+
+struct SchoolListItem: View {
+
+        let school: School
+
+        var body: some View {
+            Text(school.name)
+                .lineLimit(1)
+                .font(.system(.title3, design: .rounded))
+                .foregroundColor(.publicNavy)
+        }
+    }
+
+
+
+struct SchoolListItem_Previews: PreviewProvider {
+    static var previews: some View {
+        SchoolListItem(school: School.sampleData[0])
+    }
+}
